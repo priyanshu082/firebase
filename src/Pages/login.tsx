d57bfba708bccbaa63a0a442edece5a9391db940
priyanshu082 <priyanshusingh216@gmail.com>
2023-03-13 07:55:19 +0530

    login and logout complete

--- src/Pages/login.tsx
+++ src/Pages/login.tsx
@@ -1,10 +1,21 @@
-import {auth,provider} from "../config/firebase"
+import { auth, provider } from "../config/firebase"
+import { signInWithPopup } from "firebase/auth"
+import { useNavigate } from "react-router-dom"
 
-export const Login =()=>{
+export const Login = () => {
+const navigate=useNavigate()
+
+        const signInWithGoogle = async () => {
+            const result = await signInWithPopup(auth, provider)
+            console.log(result)
+            navigate("/")
+        }
+    
+
     return (
         <div>
             <p>Sign in with Google To Continue</p>
-            <button>Sign In</button>
+            <button onClick={signInWithGoogle}>Sign In</button>
         </div>
     )
 }
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,10 +1,30 @@
 import { Link } from "react-router-dom"
+import {auth} from "../config/firebase"
+import {useAuthState} from "react-firebase-hooks/auth"
+import {signOut} from "firebase/auth"
 
 export const Navbar = () => {
+
+    const [user]=useAuthState(auth)
+
+    const signUserOut= async ()=>{
+      await signOut(auth)
+    }
     return (
-    <div>
+    <div className="navbar">
+      <div className="links">
       <Link to="/">Home</Link>
       <Link to="/login">LogIn</Link>
+      </div>
+      <div className="user">
+        {user && (
+          <>
+        <p>{auth.currentUser?.displayName}</p>
+        <img src={auth.currentUser?.photoURL || ""} width="20" height="20"/>
+        <button onClick={signUserOut}>Log Out</button>
+        </>
+)}
+      </div>
     </div>
     )
     }
